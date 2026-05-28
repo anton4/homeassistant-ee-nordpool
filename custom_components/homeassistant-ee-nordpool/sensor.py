e4fce7872a927240c524c4da89a01679b6375ec9
Jorma Suumann <jorma@carrentalgateway.com>
2026-05-28 16:10:09 +0300

    fixing formatting

--- custom_components/homeassistant-ee-nordpool/sensor.py
+++ custom_components/homeassistant-ee-nordpool/sensor.py
@@ -107,4 +107,220 @@ class NordpoolBaseEntity(CoordinatorEntity):
             if price_type == "import":
                 is_weekend = start_dt.weekday() in (5, 6)
                 is_night_hour = start_dt.hour < 7 or start_dt.hour >= 22
-                is_holiday = start_dt.date() in ee_
+                is_holiday = start_dt.date() in ee_holidays
+
+                if is_weekend or is_night_hour or is_holiday:
+                    tariff = margin + taastuv + aktsiis + tasakaal + varustus + el_night
+                else:
+                    tariff = margin + taastuv + aktsiis + tasakaal + varustus + el_day
+
+                final_value = (p["value"] + tariff) * (1.0 + (vat_percent / 100.0))
+            else:
+                final_value = p["value"] - ex_margin - ex_tasakaal
+
+            calculated_prices.append({
+                "start": p["start"],
+                "end": p["end"],
+                "value": round(final_value, 5),
+                "is_forecast": p.get("is_forecast", False)
+            })
+
+        return calculated_prices
+
+
+class NordpoolPriceSensor(NordpoolBaseEntity, SensorEntity):
+    def __init__(self, coordinator):
+        super().__init__(coordinator)
+        self._attr_name = "Raw Prices"
+        self._attr_unique_id = "nordpool_prices_ee_sensor"
+        self._attr_icon = "mdi:lightning-bolt"
+
+    @property
+    def native_value(self):
+        prices = self._get_merged_raw_prices()
+        if prices:
+            return f"{len(prices)} periods loaded"
+        return "Waiting for data"
+
+    @property
+    def extra_state_attributes(self):
+        return {
+            "prices": self._get_merged_raw_prices(),
+            "last_poll_time": self.coordinator.last_poll_time.isoformat() if self.coordinator.last_poll_time else None
+        }
+
+class NordpoolStateSensor(NordpoolBaseEntity, SensorEntity):
+    def __init__(self, coordinator):
+        super().__init__(coordinator)
+        self._attr_name = "Prices State"
+        self._attr_unique_id = "nordpool_prices_state_sensor"
+        self._attr_icon = "mdi:list-status"
+
+    @property
+    def native_value(self):
+        return self.coordinator.data.get("state", "Waiting")
+
+    @property
+    def extra_state_attributes(self):
+        return {
+            "http_code_today": self.coordinator.data.get("http_code_today"),
+            "status_today": self.coordinator.data.get("api_status_today", "Unknown"),
+            "http_code_tomorrow": self.coordinator.data.get("http_code_tomorrow"),
+            "status_tomorrow": self.coordinator.data.get("api_status_tomorrow", "Unknown")
+        }
+
+class NordpoolImportCostSensor(NordpoolBaseEntity, SensorEntity):
+    def __init__(self, coordinator):
+        super().__init__(coordinator)
+        self._attr_name = "Import Cost"
+        self._attr_unique_id = "nordpool_import_cost_ee_sensor"
+        self._attr_icon = "mdi:transmission-tower-export"
+
+    @property
+    def native_value(self):
+        prices = self._get_calculated_prices("import")
+        if prices:
+            return f"{len(prices)} calculated"
+        return "Waiting for data"
+
+    @property
+    def extra_state_attributes(self):
+        return {"prices": self._get_calculated_prices("import")}
+
+class NordpoolExportCostSensor(NordpoolBaseEntity, SensorEntity):
+    def __init__(self, coordinator):
+        super().__init__(coordinator)
+        self._attr_name = "Export Cost"
+        self._attr_unique_id = "nordpool_export_cost_ee_sensor"
+        self._attr_icon = "mdi:transmission-tower-import"
+
+    @property
+    def native_value(self):
+        prices = self._get_calculated_prices("export")
+        if prices:
+            return f"{len(prices)} calculated"
+        return "Waiting for data"
+
+    @property
+    def extra_state_attributes(self):
+        return {"prices": self._get_calculated_prices("export")}
+
+class NordpoolFromNowSensor(NordpoolBaseEntity, SensorEntity):
+    def __init__(self, coordinator):
+        super().__init__(coordinator)
+        self._attr_name = "Prices From Now"
+        self._attr_unique_id = "nordpool_prices_from_now_sensor"
+        self._attr_icon = "mdi:chart-timeline-variant-shimmer"
+
+    @property
+    def native_value(self):
+        """State is the number of 15-minute periods left."""
+        now = dt_util.now()
+        prices = self._get_calculated_prices("import")
+        future_prices = [p for p in prices if dt_util.parse_datetime(p["end"]) > now]
+        return len(future_prices)
+
+    @property
+    def extra_state_attributes(self):
+        now = dt_util.now()
+        
+        import_prices = self._get_calculated_prices("import")
+        future_imports = [p["value"] for p in import_prices if dt_util.parse_datetime(p["end"]) > now]
+        
+        export_prices = self._get_calculated_prices("export")
+        future_exports = [p["value"] for p in export_prices if dt_util.parse_datetime(p["end"]) > now]
+
+        return {
+            "timestamps_left": len(future_imports),
+            "import_prices": future_imports,
+            "export_prices": future_exports
+        }
+
+class NordpoolSolcastSensor(NordpoolBaseEntity, SensorEntity):
+    def __init__(self, coordinator):
+        super().__init__(coordinator)
+        self._attr_name = "Solcast Forecast 15min"
+        self._attr_unique_id = "nordpool_solcast_15min_sensor"
+        self._attr_icon = "mdi:solar-power"
+
+    @property
+    def native_value(self):
+        return len(self._get_solcast_forecast())
+
+    @property
+    def extra_state_attributes(self):
+        return {
+            "values": self._get_solcast_forecast()
+        }
+
+    def _get_solcast_forecast(self):
+        hass = self.coordinator.hass
+        now = dt_util.now()
+        
+        field_select = hass.states.get('select.solcast_pv_forecast_use_forecast_field')
+        field_name = field_select.state if field_select else 'estimate'
+        forecast_use_field = f"pv_{field_name}"
+
+        sensors = [
+            "sensor.solcast_pv_forecast_forecast_today",
+            "sensor.solcast_pv_forecast_forecast_tomorrow",
+            "sensor.solcast_pv_forecast_forecast_day_3",
+            "sensor.solcast_pv_forecast_forecast_day_4",
+            "sensor.solcast_pv_forecast_forecast_day_5",
+            "sensor.solcast_pv_forecast_forecast_day_6",
+            "sensor.solcast_pv_forecast_forecast_day_7"
+        ]
+
+        raw_30min_values = []
+        for sensor_id in sensors:
+            state = hass.states.get(sensor_id)
+            if state and 'detailedForecast' in state.attributes:
+                for item in state.attributes['detailedForecast']:
+                    val = item.get(forecast_use_field, 0.0)
+                    raw_30min_values.append(val)
+
+        values_15min_watts = []
+        dampener = 1
+        for val in raw_30min_values:
+            watts = int(float(val) * 1000) * dampener
+            values_15min_watts.extend([watts, watts])
+
+        intervals_since_midnight = (now.hour * 4) + (now.minute // 15)
+        
+        prices = self._get_calculated_prices("import")
+        future_prices = [p for p in prices if dt_util.parse_datetime(p["end"]) > now]
+        np_range = len(future_prices)
+
+        sliced_values = values_15min_watts[intervals_since_midnight : intervals_since_midnight + np_range]
+
+        while len(sliced_values) < np_range:
+            sliced_values.append(0)
+
+        return sliced_values
+
+
+class NordpoolLastPollSensor(NordpoolBaseEntity, SensorEntity):
+    _attr_device_class = SensorDeviceClass.TIMESTAMP
+
+    def __init__(self, coordinator):
+        super().__init__(coordinator)
+        self._attr_name = "Last Poll Time"
+        self._attr_unique_id = "nordpool_last_poll_time_sensor"
+        self._attr_icon = "mdi:clock-check"
+
+    @property
+    def native_value(self):
+        return self.coordinator.last_poll_time
+
+class NordpoolNextPollSensor(NordpoolBaseEntity, SensorEntity):
+    _attr_device_class = SensorDeviceClass.TIMESTAMP
+
+    def __init__(self, coordinator):
+        super().__init__(coordinator)
+        self._attr_name = "Next Poll Time"
+        self._attr_unique_id = "nordpool_next_poll_time_sensor"
+        self._attr_icon = "mdi:update"
+
+    @property
+    def native_value(self):
+        return self.coordinator.next_poll_time
